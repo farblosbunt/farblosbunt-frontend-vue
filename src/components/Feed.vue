57338e36b3919001dcfc82d8f506628c3a59f797
Felix Kopp <fk@claptec.de>
2017-03-04 15:58:04 +0100

    Fixed Sapces

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="feed">
     <nav>
-    <div class="nav-wrapper center-align" style="background-color:#f5f5f5">
-      <img class="left" src="../assets/logo.png" height="40px" style="margin-left:20px; margin-top: 10px">
-      <input placeholder="SUCHE" v-model="query" type="search" style="margin-top:0px; width:350px; text-align: center; font-size: 25px; color:black; margin:auto">
+      <div class="nav-wrapper center-align" style="background-color:#f5f5f5">
+        <img class="left" src="../assets/logo.png" height="40px" style="margin-left:20px; margin-top: 10px">
+        <input placeholder="SUCHE" v-model="query" type="search" style="margin-top:0px; width:350px; text-align: center; font-size: 25px; color:black; margin:auto">
+      </div>
+    </nav>
+    <div class="col l12 center-align" style="margin-top:20px; margin-bottom:15px">
+      <topic-chip v-for="topic in trendingTopics" :topic="topic" :key="topic.id"></topic-chip>
     </div>
-  </nav>
-  <div class="col l12 center-align" style="margin-top:20px; margin-bottom:15px">
-    <topic-chip v-for="topic in trendingTopics" :topic="topic" :key="topic.id"></topic-chip>
-  </div>
     <div class="row">
       <feed-stream feedType="liberal" class="col s6 no-padding center-align"></feed-stream>
       <feed-stream feedType="conservative" class="col s6 no-padding center-align"></feed-stream>
