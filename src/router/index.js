18b71c4ad911406475834d8e9184c152a4c43d1e
Felix Kopp <fk@claptec.de>
2017-03-04 10:57:28 +0100

    Router added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Start from '@/components/Start'
+import Feed from '@/components/Feed'
 
 Vue.use(Router)
 
@@ -8,8 +9,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Start',
+      component: Start
+    },
+    {
+      path: '/feed/:term',
+      name: 'Feed',
+      component: Feed,
+      props: true
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
