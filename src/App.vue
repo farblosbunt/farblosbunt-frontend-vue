c8c125a114469077510ed1cadff75cacbcc51a0f
Felix Kopp <fk@claptec.de>
2017-03-04 12:10:45 +0100

    Integrated Facebook SDK

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="app">
-  	<router-view></router-view>
+    <div id="fb-root"></div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted: function () {
+    /* eslint-disable */
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml      : true,
+        version    : 'v2.8'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/de_DE/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+    /* eslint-enable */
+  }
 }
 </script>
 
